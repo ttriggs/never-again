8e570c0a66425b65f397bb91b35b0d79821be0f4
Frank B <fb.fbook@gmail.com>
2015-03-31 15:37:19 -0400

    Last blank line removed

--- spec/features/users/user_signs_up_spec.rb
+++ spec/features/users/user_signs_up_spec.rb
@@ -8,7 +8,6 @@ feature "user registers", %{
   scenario "provide valid registration information" do
     visit new_user_registration_path
 
-
     fill_in "Email", with: "john@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
