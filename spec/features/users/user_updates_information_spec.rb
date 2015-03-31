12211b41a75b2fbf012e097662b77c9500dfef91
Frank B <fb.fbook@gmail.com>
2015-03-31 15:35:29 -0400

    Still finding and fixing errors

--- spec/features/users/user_updates_information_spec.rb
+++ spec/features/users/user_updates_information_spec.rb
@@ -4,21 +4,20 @@ feature "user updates user information", %{
   As a signed in user, I want to be able to
   update my user information } do
   scenario "update user information" do
-    user1 = FactoryGirl.create(:user)
-    user2 = FactoryGirl.create(:user)
+    user = FactoryGirl.create(:user)
 
     visit root
-    click_button "Sign in"
-    fill_in "Email", with: user1.email
-    fill_in "Password", with: user1.password
+
+    sign_in_as user
 
     click_button "My account"
-    fill_in "Email", with: user2.email
-    fill_in "Password", with: user2.email
+
+    fill_in "Email", with: "pops@aol.com"
+    fill_in "Password", with: "12345678"
 
     click_button "Save changes"
 
-    expect(page).to have_content(user2.email)
-    expect(page).to have_content(user2.password)
+    expect(page).to have_content("pops@aol.com")
+    expect(page).to have_content("12345678")
   end
 end
