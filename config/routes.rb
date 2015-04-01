1f08b4312c78763606d7d7d9755516927e48ae1f
ttriggs <tyler.triggs@gmail.com>
2015-04-01 15:58:21 -0400

    minor tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => :registrations }
+  devise_for :users, controllers: { registrations: :registrations }
   root 'restaurants#index'
   resources :restaurants do
     resources :reviews, only: [:new, :create]
