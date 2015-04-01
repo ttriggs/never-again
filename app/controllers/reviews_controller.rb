1c9551d5305fb8ba0cb949f4e5c3002b1cb214e2
Mafooch <jpmaff@yahoo.com>
2015-04-01 16:25:08 -0400

    add features

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
@@ -18,10 +19,31 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
+  def edit
+    @review = current_user.reviews.find(params[:id])
+    @restaurant = @review.restaurant
+  end
+
+  def update
+    @review = Review.update(params[:id], review_params)
+    if @review.save
+      flash[:notice] = "Review edited."
+      redirect_to @review.restaurant
+    else
+      flash[:notice] = "Update failed"
+      render :new
+    end
+  end
+
+  def destroy
+    @review = current_user.reviews.find(params[:id])
+    @review.destroy
+    redirect_to restaurants_path, notice: 'Review deleted.'
+  end
+
   private
 
   def review_params
     params.require(:review).permit(:rating, :body)
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'restaurants#index'
   resources :restaurants do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:show, :new, :create, :edit, :update]
   end
-  resources :reviews, only: [:edit, :update, :destroy]
+  resources :reviews, only: [:destroy]
 end
