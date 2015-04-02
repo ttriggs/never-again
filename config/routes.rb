87ddf694209ac2ae0fc178d23971e7c4528bf041
Mafooch <jpmaff@yahoo.com>
2015-04-02 15:51:06 -0400

    no change

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   root 'restaurants#index'
   resources :restaurants do
     resources :reviews, only: [:show, :new, :create, :edit, :update]
-    resources :upvotes, only: :create
-    resources :downvotes, only: :create
   end
   resources :users, only: [:show]
   resources :reviews, only: [:destroy]
