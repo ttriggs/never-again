18a31433a77400bfebc360a2fb35806a3723bd67
Mafooch <jpmaff@yahoo.com>
2015-04-06 14:22:28 -0400

    refactor test

--- spec/features/restaurants/view_restaurant_spec.rb
+++ spec/features/restaurants/view_restaurant_spec.rb
@@ -3,23 +3,32 @@ require 'rails_helper'
 feature 'view restaurants' do
   context 'as an authorized user' do
     let(:user) { FactoryGirl.create(:user) }
+    let(:next_page_restaurant) do
+      FactoryGirl.create(:restaurant, name: "first restaurant")
+    end
+
 
     before :each do
       sign_in_as user
+      next_page_restaurant
     end
 
-    scenario 'I can see all restaurants on their appropriate pages i.e. paginate
-      works' do
-      next_page_restaurant = FactoryGirl.create(
-        :restaurant, name: "first restaurant"
-        )
-      restaurants = Array.new
-      25.times { |r| restaurants << FactoryGirl.create(:restaurant) }
+    scenario 'I can see all restaurants on the first page' do
+      25.times { FactoryGirl.create(:restaurant) }
 
       visit restaurants_path
-      expect(page).to have_link(restaurants.last.name)
+      expect(page).to have_link(Restaurant.all.last.name)
       expect(page).to_not have_link(next_page_restaurant.name)
 
+      click_link "Next"
+
+      expect(page).to have_link(next_page_restaurant.name)
+    end
+
+    scenario 'I can see all restaurants on the next page' do
+      25.times { FactoryGirl.create(:restaurant) }
+
+      visit restaurants_path
       click_link "Next"
 
       expect(page).to have_link(next_page_restaurant.name)
--- spec/features/review/user_views_reviews_spec.rb
+++ spec/features/review/user_views_reviews_spec.rb
@@ -4,21 +4,30 @@ feature "View all reviews for a restaurant", %q(
   As an user
   I want to view all reviews for a given restaurant
   so I can see just how bad the restaurant is
-),focus: true do
+) do
+    let(:restaurant) { FactoryGirl.create(:restaurant) }
+    let(:next_page_review) do
+      FactoryGirl.create( :review,
+      restaurant: restaurant, body: "this review is on the next page")
+    end
 
-  restaurant = FactoryGirl.create(:restaurant)
-  next_page_review = FactoryGirl.create(
-    :review, restaurant: restaurant, body: "this review is on the next page"
-  )
-  10.times { reviews << FactoryGirl.create(:review, restaurant: restaurant) }
+    before :each do
+      next_page_review
+      10.times { FactoryGirl.create(:review, restaurant: restaurant) }
+    end
 
-  scenario "user views all reviews on the first page for a restaurant" do
-    visit restaurant_path(restaurant)
-    expect(page).to have_content(Review.all.first.rating)
-    expect(page).to_not have_content(next_page_review.body)
+    scenario "user views all reviews on the first page for a restaurant" do
+      visit restaurant_path(restaurant)
 
-    click_link "Next"
-    expect(page).to have_content(next_page_review.body)
-    expect(page).to_not have_content(Review.all.first.body)
-  end
+      expect(page).to have_content(Review.all.first.rating)
+      expect(page).to_not have_content(next_page_review.body)
+    end
+
+    scenario "user views all reviews on the second page for a restaurant" do
+      visit restaurant_path(restaurant)
+      click_link "Next"
+
+      expect(page).to have_content(next_page_review.body)
+      expect(page).to_not have_content(Review.all.last.body)
+    end
 end
