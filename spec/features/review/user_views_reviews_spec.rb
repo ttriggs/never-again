af3ad59207ed93dbb615d508c91a9727276c8d87
Mafooch <jpmaff@yahoo.com>
2015-04-06 12:13:17 -0400

    nothing

--- spec/features/review/user_views_reviews_spec.rb
+++ spec/features/review/user_views_reviews_spec.rb
@@ -4,24 +4,21 @@ feature "View all reviews for a restaurant", %q(
   As an user
   I want to view all reviews for a given restaurant
   so I can see just how bad the restaurant is
-) do
+),focus: true do
 
-  scenario "user views all reviews for a restaurant on their appropriate
-  pages" do
-    restaurant = FactoryGirl.create(:restaurant)
-    next_page_review = FactoryGirl.create(
-      :review, restaurant: restaurant, body: "this review is on the next page"
-    )
-    10.times do |r|
-      reviews << FactoryGirl.create(:review, restaurant: restaurant)
-    end
+  restaurant = FactoryGirl.create(:restaurant)
+  next_page_review = FactoryGirl.create(
+    :review, restaurant: restaurant, body: "this review is on the next page"
+  )
+  10.times { reviews << FactoryGirl.create(:review, restaurant: restaurant) }
 
+  scenario "user views all reviews on the first page for a restaurant" do
     visit restaurant_path(restaurant)
-    expect(page).to have_content(reviews.last.rating)
+    expect(page).to have_content(Review.all.first.rating)
     expect(page).to_not have_content(next_page_review.body)
 
     click_link "Next"
     expect(page).to have_content(next_page_review.body)
-    expect(page).to_not have_content(reviews.last.body)
+    expect(page).to_not have_content(Review.all.first.body)
   end
 end
