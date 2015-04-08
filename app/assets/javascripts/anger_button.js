7387786c980c83f8e1c7aa13a249c279ddf4adfe
Mafooch <jpmaff@yahoo.com>
2015-04-08 15:37:20 -0400

    refactor

--- app/assets/javascripts/anger_button.js
+++ app/assets/javascripts/anger_button.js
@@ -1,7 +1,7 @@
-var startingBackgroundColor = $("body").css('background-color')
+var startingBackgroundColor = $("body").css("background-color");
 var rgbArray = startingBackgroundColor.substring(
   4, startingBackgroundColor.length - 1).split(", "
-  )
+);
 var r = parseInt(rgbArray[0]);
 var g = parseInt(rgbArray[1]);
 var b = parseInt(rgbArray[2]);
@@ -35,9 +35,8 @@ $("#rage-button").click(function(){
     incrementRage();
     g = g - 5;
     b = b - 5;
-    rgb = "rgb(" + r + ", " + g + ", " + b + ")";
-    var test = $("#rage-button").css("background-color")
-    $("body").css('background-color', rgb);
+    var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+    $("body").css("background-color", rgb);
     $("#rage-button").text(rageLevel());
     $("#review_rage_level").val(rageLevel);
 });
